c35b9223c370b4af57da63c67ac8c13909640f7b
Eric Born <eric.born85@gmail.com>
2023-02-18 01:20:18 -0600

    Update items.py

--- items.py
+++ items.py
@@ -43,12 +43,46 @@ while equal_count < 3:
 # remove first two and last characters since they are all equal signs
 item_cat = item_cat[2:len(item_cat)-1]
 
-# find all items like |link=xxx]]
-text_split = text.split(r'|link=')
+# split text on |link=
+item_split = text.split(r'|link=')
 
 # setup then populate a list from the 0 element of each item in text_split
 # which is split again on the first closed bracket symbol ]
 item_list = []
 
-for items in range(1, len(text_split)):
-    item_list.append(text_split[items].split(r']')[0])
+for items in range(1, len(item_split)):
+    item_list.append(item_split[items].split(r']')[0])
+
+# TODO
+# need to create a dataframe, store values from item_list then append
+# the following types below
+
+# other 
+item_list[0:27]
+
+# building materials
+item_list[27:44]
+
+# electronics
+item_list[44:89]
+
+# energy elements
+item_list[89:97]
+
+# Flammable materials
+item_list[97:112]
+
+# Household materials
+item_list[112:129]
+
+# medical supplies
+item_list[129:140]
+
+# tools
+item_list[140:160]
+
+# valuables
+item_list[160:182]
+
+# list got cut here for some reason, need to resolve as there are two more
+# groups of items
